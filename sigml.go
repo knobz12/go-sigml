5d49629cfbdbac6aefd187266fcb7d91941da179
knobz12 <mjojla@hotmail.com>
2024-12-13 03:21:29 +0100

    Formatting and more
    Formatted some code, added severity to all constants and added omitempty to "n" value/sensor name. Also changed to small letter on error messages following the go static check (ST1005) standard.

--- sigml.go
+++ sigml.go
@@ -1,33 +1,30 @@
 package sigml
 
-import (
-	"errors"
-	//"fmt"
-	//"time"
-)
+import "errors"
 
 type SigMLRecord struct {
-	Bn string `json:"bn,omitempty"`  //source device/system
-	Bt float64 `json:"bt,omitempty"`
-	Bver int `json:"bver,omitempty"`
-	N string `json:"n"`  //source service/signal
+	Bn   string  `json:"bn,omitempty"`   //source device/system
+	Bt   float64 `json:"bt,omitempty"`   // Basetime linux epoch
+	Bver int     `json:"bver,omitempty"` // Version
+	N    string  `json:"n,omitempty"`    //source service/signal
 
-	X string  `json:"x,omitempty"` // eXception
-	E string  `json:"e,omitempty"` // Eror
-	S string  `json:"s,omitempty"` // Signal/event
-	Se Severity `json:"se,omitempty"`
-	D string `json:"d,omitempty"`  // Description
-	P interface{} `json:"p,omitempty"` //payload
+	X  string      `json:"x,omitempty"`  // Exception
+	E  string      `json:"e,omitempty"`  // Error
+	S  string      `json:"s,omitempty"`  // Signal/event
+	Se Severity    `json:"se,omitempty"` // Severity level
+	D  string      `json:"d,omitempty"`  // Description
+	P  interface{} `json:"p,omitempty"`  // Payload
 }
 
 type Severity uint8
+
 const (
-	None Severity = 0
-	Normal        = 1
-	Incident      = 50
-	Error         = 100
-	Critical      = 200
-	Catastrophic  = 255
+	None         Severity = 0
+	Normal       Severity = 1
+	Incident     Severity = 50
+	Error        Severity = 100
+	Critical     Severity = 200
+	Catastrophic Severity = 255
 )
 
 type SigMLMessage []SigMLRecord
@@ -35,23 +32,22 @@ type SigMLMessage []SigMLRecord
 // Validate tests if a SigML message is well formated
 func (msg SigMLMessage) Validate() error {
 	if len(msg) == 0 {
-		return errors.New("Empty message")
+		return errors.New("empty message")
 	}
 
 	for _, rec := range msg {
 		if rec.X == "" && rec.E == "" && rec.S == "" {
-			return errors.New("One of x, e or s must be valid")
+			return errors.New("one of x, e or s must be valid")
 		}
 		if rec.X != "" && rec.E != "" && rec.S != "" {
-			return errors.New("Only one of x, e or s can be valid")
+			return errors.New("only one of x, e or s can be valid")
 		}
 		if rec.X != "" && (rec.E != "" || rec.S != "") {
-			return errors.New("Only one of x, e or s can be valid")
+			return errors.New("only one of x, e or s can be valid")
 		}
 		if (rec.X != "" || rec.E != "") && rec.S != "" {
-			return errors.New("Only one of x, e or s can be valid")
+			return errors.New("only one of x, e or s can be valid")
 		}
 	}
 	return nil
 }
-
